e47bd69be4af0d6f6c08272951553d5b7b2f44bc
gm-costa <gmaia_01@hotmail.com>
2023-01-09 17:12:30 -0300

    Cadastro usuário finalizado

--- adote/settings.py
+++ adote/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def cadastro(request):
     if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            messages.add_message(request, messages.WARNING, 'Preencha todos os campos!')
+            return render(request, 'cadastro.html')
+        
+        if senha != confirmar_senha:
+            messages.add_message(request, messages.ERROR, 'As senhas não são iguais.')
+            return render(request, 'cadastro.html')
+
+        try:
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha,
+            )
+            messages.add_message(request, messages.SUCCESS, 'Usuário cadastrado com sucesso.')
+            # return render(request, 'cadastro.html')
+        except:
+            messages.add_message(request, messages.ERROR, 'Ocorreu um erro ao tentar salvar, tente novamente!')
+            
         return render(request, 'cadastro.html')
