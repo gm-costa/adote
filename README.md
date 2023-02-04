4dc53b98752fad4371a40132765212e7b134f015
gm-costa <gmaia_01@hotmail.com>
2023-02-04 08:16:46 -0300

    Adiciona decouple

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Projeto desenvolvido na PSW2023 - O RETORNO, da pythonando.
 
 ## Requisitos:
 
-Python e Pillow
+Python, Pillow e decouple
 
 ## Como usar:
 
 - Clonar o projeto
+- Renomear o arquivo .env_sample para .env, e informar sua secret_key no mesmo
 - Executar o makemigrations e o migrate
 - Executar o runserver
 - No browse, inserir a seguinte url:  http://127.0.0.1:8000/auth/logar/
--- adote/settings.py
+++ adote/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*g1)olt%6f$4+j)*p6xj%-3u853v+*mgmwxt7524lc$n+i97=q"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
